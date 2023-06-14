5f46b5268a523f8dc2b33191816d2a15cb1c8d69
Donald Soh <sohdonald@gmail.com>
2023-06-14 16:57:05 -0400

    challenge 2 complete

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
     makeImg.src = element;
 
     const getDogImg = document.querySelector("#dog-image-container");
+    // querySelector gets the html element with
+    // the id of dog-image-container
+
     // append is not working
     // we want to append the image to the html
 
@@ -31,7 +34,64 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
 imgUrl();
 // how do we get each picture from the fetch url?
 // images are in message key
-//imgUrl.querySelector();
+
+function breedUrl () {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    //url is the path to the resource you want to fetch
+    // fetch gets the data inside the resource
+    
+    .then((res) => res.json())
+    // res is the response
+    // response is sent back as json
+    // converts json into js objects
+
+    .then( (data) => {
+
+      //we need a preventDefault here
+      // submit action causes refresh
+      // form elements use submit
+      // we're not submitting form
+        //e.preventDefault();
+        // do not want white screen of death
+
+    const getDogBreeds = document.querySelector("#dog-breeds")
+        // querySelector gets the html element with
+        // id= dog-breeds
+        //adds the breeds to the <ul>
+        console.log(getDogBreeds);
+    
+        // need to add the dog breeds to the ul
+        // we need html elements
+        // data needs html elements to display info
+        // need to create li elements
+        // li are list elements
+        // displayed inside of the html element ul
+        //.createElement is needed
+        //.forEach is needed too
+        //.forEach is for arrays
+        // transform the keys in the object
+        // into elements in an array
+        const makeDogArray = Object.keys(data.message);
+        // Object with big O is global object in js
+        console.log(makeDogArray);
+        makeDogArray.forEach((dogBreed) => {
+            const makeDogLiElement = document.createElement("li");
+            console.log(dogBreed);
+            console.log(makeDogLiElement);
+            //setAtttribute the li's textcontent
+            makeDogLiElement.textContent = dogBreed;
+
+            // need append to add to html, we need an existing element
+            getDogBreeds.append(makeDogLiElement);
+        });
+        
+    console.log(data);
+    }
+    ) // second .then ends
+} //breedUrl ends
+
+breedUrl();
+
 
 
 
