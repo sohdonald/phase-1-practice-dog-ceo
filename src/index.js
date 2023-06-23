1d03f5cf7213018f7c82f739b83e267d2c2f3d9b
Donald Soh <sohdonald@gmail.com>
2023-06-22 20:17:12 -0400

    want to make filterBreeds visible

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 document.addEventListener("DOMContentLoaded", 
 () => {
 
+let arrayCopy
+
 console.log("%c HI", "color: firebrick");
 
 function imgUrl () {
@@ -47,19 +49,20 @@ function breedUrl () {
 
     .then( (data) => {
 
-      //we need a preventDefault here
-      // submit action causes refresh
+      //do we need a preventDefault here?
+      // submit action causes refresh by default
+      // do not want white screen of death
       // form elements use submit
       // we're not submitting form
-        //e.preventDefault();
-        // do not want white screen of death
+        //e.preventDefault(); is not needed
+        
 
     const getDogBreeds = document.querySelector("#dog-breeds")
         // querySelector gets the html element with
         // id= dog-breeds
         //adds the breeds to the <ul>
         console.log(getDogBreeds);
-    
+        
         // need to add the dog breeds to the ul
         // we need html elements
         // data needs html elements to display info
@@ -72,20 +75,24 @@ function breedUrl () {
         // transform the keys in the object
         // into elements in an array
         const makeDogArray = Object.keys(data.message);
+        arrayCopy = [...makeDogArray]
         // Object with big O is global object in js
         console.log(makeDogArray);
         makeDogArray.forEach((dogBreed) => {
             const makeDogLiElement = document.createElement("li");
-            console.log(dogBreed);
-            console.log(makeDogLiElement);
+            //console.log(dogBreed);
+            //console.log(makeDogLiElement);
             //setAtttribute the li's textcontent
             makeDogLiElement.textContent = dogBreed;
-
-            // need append to add to html, we need an existing element
-            getDogBreeds.append(makeDogLiElement);
-
-        }); // forEach ends
-        
+            makeDogLiElement.addEventListener("click", () => {
+                makeDogLiElement.style.color = "red"; 
+                // red color doesn't persist after refreshing website
+           }) //addEventListener ends
+           // is this organized?
+            // need append to add to html, we need an existing element    
+            getDogBreeds.append(makeDogLiElement)
+           // .append should be the last thing here
+        }); // forEach ends        
     console.log(data);
     }) // second .then ends
 } //breedUrl ends
@@ -96,14 +103,41 @@ breedUrl();
 // what should we call the addEventListener to?
 // most likely the li we just made
 
-
 //li.addEventListener("click", (e) => {
-
+ //   .getElementById().style.color = "red";
 //});
 
+// should look something like this
+
+// where should this code be?
+
 // need to code css inside js to change font color
+// we can do this with .getElementById().style.color = "red";
 
+// Challenge 4 is asking us to filter
+// need to use filter method
+// make a dropdown
+/* note that there is a <select> html element with
+the id "#breed-dropdown"
 
+we want <option value="a"> to filter the ul with
+only the breeds starting with letter "a"
+*/
 
+const breedDropdown = document.querySelector("#breed-dropdown")
+//console.log(breedDropdown)
+breedDropdown.addEventListener("change", (e) => {
+    //console.log(e.target)
+    const breedTarget = e.target.value
+    const filterBreeds = arrayCopy.filter((breed) => {
+        // return breeds that start with breedTarget
+        return breed.charAt(0) === breedTarget
+        // need to append something, but what and where?
+    })
+    console.log(filterBreeds)
 
+    // breedTarget gets the letter from the option
+    
+    
+})
 }) // DOMContentLoaded ends
